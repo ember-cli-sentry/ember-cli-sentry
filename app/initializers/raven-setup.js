ffa73d7e4667a8c4ce7ac2c74bcf226307e7638b
Damien Caselli <damien.caselli@gmail.com>
2015-02-04 14:31:44 +0100

    Add Ember.onerror hook

--- app/initializers/raven-setup.js
+++ app/initializers/raven-setup.js
@@ -1,11 +1,22 @@
 /* global Raven */
 
 import config from '../config/environment';
+import Ember from 'ember';
 
 export function initialize() {
+  var _onerror;
+
   Raven.config(config.sentry.dsn, {
     whitelistUrls: config.sentry.whitelistUrls
   }).install();
+
+  _onerror = Ember.onerror;
+  Ember.onerror = function(error) {
+    Raven.captureException(error);
+    if (typeof _onerror === 'function') {
+      _onerror.call(this, error);
+    }
+  }
 }
 
 export default {
