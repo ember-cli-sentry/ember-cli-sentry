2e53514ea2d802236a810cc7588d2934132978f2
Aaron Chambers <achambers@gmail.com>
2016-01-15 15:54:37 +0000

    Add a default blueprint that will install raven-js

--- index.js
+++ index.js
@@ -4,6 +4,12 @@
 module.exports = {
   name: 'ember-cli-sentry',
 
+  included: function(app) {
+    this._super.included(app);
+
+    app.import(app.bowerDirectory + '/raven-js/dist/raven.min.js');
+  },
+
   contentFor: function(type, config) {
     if (type === 'body-footer' && config.sentry && !config.sentry.development && config.sentry.cdn) {
       return '<script src="' + config.sentry.cdn + '"></script>';
