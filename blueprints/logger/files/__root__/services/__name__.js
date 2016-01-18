7c7dd5e2f4cb2362a2f3dc4c3169c21916be0197
Damien Caselli <damien.caselli@gmail.com>
2016-01-18 14:18:11 +0100

    Remove unused variable

--- blueprints/logger/files/__root__/services/__name__.js
+++ blueprints/logger/files/__root__/services/__name__.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
 import RavenLogger from 'ember-cli-sentry/services/raven';
 
-const { computed } = Ember;
-
 export default RavenLogger.extend({
 
   unhandledPromiseErrorMessage: '',
