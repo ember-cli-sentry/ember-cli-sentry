d7e05b50c6dba87584126976d72d219ec301ba9b
Jake Howerton <jake@zipmark.com>
2016-04-05 14:01:32 -0400

    switch to Ember.assign (Ember.merge deprecated)

--- app/instance-initializers/raven-setup.js
+++ app/instance-initializers/raven-setup.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 import config from '../config/environment';
 
+// Ember merge is deprecated as of 2.5, but we need to check for backwards
+// compatibility.
+const assign = Ember.assign || Ember.merge;
+
 export function initialize(application) {
 
   if (Ember.get(config, 'sentry.development') === true) {
@@ -29,7 +33,7 @@ export function initialize(application) {
     window.Raven.debug = debug;
 
     // Keeping existing config values for includePaths, whitelistUrls, for compatibility.
-    const ravenConfig = Ember.merge({
+    const ravenConfig = assign({
       includePaths,
       whitelistUrls,
       release: service.get(serviceReleaseProperty) || config.APP.version
