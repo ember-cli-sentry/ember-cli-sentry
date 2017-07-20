063fa76d32fc3dc2cd16c0dab52f04261b6d0b2a
orf <tom@tomforb.es>
2017-07-20 15:38:48 +0100

    Update application.js

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -6,7 +6,7 @@ export default Route.extend({
 
   captureMessageOnActivate: on('activate', function() {
     this.get('raven').callRaven('setUserContext', { id: 'abc12345', name: 'The mighty tester' });
-    this.get('raven').captureBreadcrumb('Breadcrumb captured from ApplicationRoute');
+    this.get('raven').captureBreadcrumb({message: 'Breadcrumb captured from ApplicationRoute'});
     this.get('raven').captureMessage('Message captured from ApplicationRoute');
     this.get('raven').captureException(new Error('Exception captured from ApplicationRoute'));
   })
