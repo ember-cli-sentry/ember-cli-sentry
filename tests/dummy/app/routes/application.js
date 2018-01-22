62b022d09f680176f96027b85627bb54cfe83111
Jan Buschtöns <jan@buschtoens.me>
2018-01-22 11:50:45 +0100

    fix(tests/dummy): use correct import for route

--- tests/dummy/app/routes/application.js
+++ tests/dummy/app/routes/application.js
@@ -1,4 +1,4 @@
-import Route from '@ember/route';
+import Route from '@ember/routing/route';
 import { get } from '@ember/object';
 import { on } from '@ember/object/evented';
 import { inject as service } from '@ember/service';
