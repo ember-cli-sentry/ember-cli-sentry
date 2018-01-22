b9c06818a83c474d4b1dd0e9f95a46f7c06964c5
Jan Buschtöns <jan@buschtoens.me>
2018-01-22 11:56:40 +0100

    fix(tests/test-helper): use old setResolver

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,6 @@
-import Application from '../app';
-import config from '../config/environment';
-import { setApplication } from '@ember/test-helpers';
-import { start } from 'ember-qunit';
+import resolver from './helpers/resolver';
+import { setResolver } from '@ember/test-helpers';
+import { start } from 'ember-cli-qunit';
 
-setApplication(Application.create(config.APP));
-
+setResolver(resolver);
 start();
