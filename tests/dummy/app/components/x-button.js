24f9a7aee703b5394a21a8bcb474be66f7407ae1
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-02-06 12:51:35 +0100

    Update `ember-cli` to v2.18.2

--- tests/dummy/app/components/x-button.js
+++ tests/dummy/app/components/x-button.js
@@ -10,6 +10,6 @@ export default Component.extend({
     this.get('raven').captureMessage('Message captured from test component');
     this.get('raven').captureException(new Error('Message captured from test component'));
 
-    this.attrs.afterClick('x-button clicked');
+    this.afterClick('x-button clicked');
   }
 });
