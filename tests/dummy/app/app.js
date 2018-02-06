4f520074de1c8c53293b3933d5ec02cfcd788938
Tobias Bieniek <tobias.bieniek@gmail.com>
2018-02-06 12:44:30 +0100

    Update `ember-cli` to v2.14.2

--- tests/dummy/app/app.js
+++ tests/dummy/app/app.js
@@ -1,15 +1,10 @@
-import Ember from 'ember';
 import Application from '@ember/application';
 import loadInitializers from 'ember-load-initializers';
 
 import Resolver from './resolver';
 import config from './config/environment';
 
-let App;
-
-Ember.MODEL_FACTORY_INJECTIONS = true;
-
-App = Application.extend({
+const App = Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
   Resolver
