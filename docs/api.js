8ce620bef2438b71576ed88023b4570af21048f8
dashdashzako <d@dashdashzako.net>
2018-09-12 09:53:49 +0200

    Update documentation generation
    Documentation should be easier to update, and using different pipeline
    for this step seems to be a better choice than the old one.
    
    I added `ember-cli-yuidoc` and an npm script to allow easy generation.
    
    As for the theme, I choose to use `yuidoc-ember-cli-theme` used for the
    `ember-cli` [API documentation](https://ember-cli.com/api/).
    
    I also think there is no need to split code and documentation. They can
    both coexist in the same branch, and documentation will be versioned with
    the code. The only side-effect is to configure this repository to use
    the `docs` directory for GitHub pages.

---
+++ docs/api.js
@@ -0,0 +1,18 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "RavenService"
+    ],
+    "modules": [
+        "ember-cli-sentry_services_raven"
+    ],
+    "allModules": [
+        {
+            "displayName": "ember-cli-sentry/services/raven",
+            "name": "ember-cli-sentry_services_raven",
+            "description": "Default available logger service.\n\nYou can simply extend or export this Service to use it in the application."
+        }
+    ],
+    "elements": []
+} };
+});
